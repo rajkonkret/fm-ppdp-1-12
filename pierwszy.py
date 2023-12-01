6bc0a1f9c08e85633df2f762ef450473a617cb4f
rajko <rajkonkret660@gmail.com>
2023-12-01 12:00:04 +0100

    Initial commit

---
+++ pierwszy.py
@@ -0,0 +1,36 @@
+print()  # wypisz/wydrukuj
+print("Nazywam się Radek")
+print('Nazywam się Radek')
+print('Nazywam się Radek')
+print('Nazywam się Radek')
+print('Nazywam się Radek')
+print('Nazywam się Radek')
+# crtl d - powielenie linii z kursorem
+# ctrl alt l
+print(type('Radek'))
+print(type("Radek"))
+# <class 'str'>
+# <class 'str'>
+# ctrl / - automatyczny komentarz
+
+print(39)
+print(type(39))  # <class 'int'>
+
+print("39" + "14")  # 3914 konkatenacja - łączenie stringów
+print(5 * "4")
+print(39 + 15)  # 54
+
+imie: str = "Radek"
+print(imie)
+print(type(imie))
+imie: str = 39
+print(type(imie))  # <class 'int'>
+imie = "Radek"
+imie = "39"
+wiek = 48
+print(type(wiek))  # <class 'int'>
+print(int(imie) + wiek)  # int() - rzutowanie na int
+# print("14" + 50)  # TypeError: can only concatenate str (not "int") to str
+
+wiek = "Radek"
+print(type(wiek))
