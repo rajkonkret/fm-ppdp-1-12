1d96961f3e21a84d94de8c9b4ee9d976fce07a8f
rajko <rajkonkret660@gmail.com>
2023-12-04 15:34:05 +0100

    2 - add wyjatki

---
+++ wyjatki.py
@@ -0,0 +1,27 @@
+try:
+    # print(5 / 0)  # ZeroDivisionError: division by zero
+    # print("a" / 2)
+    # print(10 + "2")
+    # print(int("A"))
+    print(1 + 2)
+except ZeroDivisionError:
+    print("Nie dziel prze zero")
+except TypeError:
+    print("Bład typu")
+except ValueError:
+    print("Bład wartości")
+except Exception as e:
+    print("Bład", e)
+else:
+    print("Tylko gdy nie ma błedu")
+finally:
+    print("Wykona się niezależnie czy błąd wystąpi czy nie")
+
+print("Dalsza cześć programy")
+# try - except [else,finally]
+# program kalkulator
+# ma wyswietlic menu - dodawanie, odejmowanie, mnozenie, dzielenie
+# ma uzyc petli while
+# moze byc if lu match case
+# ma ładnie wydrukować wynik działania
+# Dodawanie 1 + 6 = 7
