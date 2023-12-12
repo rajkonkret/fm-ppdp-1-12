3d6ed0c748d009ab68b596eb79c175c388735125
radoslaw.janiak <radoslaw.janiak@softpos.eu>
2023-12-12 10:54:43 +0100

    4 - update baza4

--- baza3.py
+++ baza3.py
@@ -6,10 +6,15 @@ try:
     insert = '''
     INSERT INTO software (id,name,price) VALUES(1,'Python', 200);
     '''
+    select = '''
+    SELECT * FROM software
+    '''
     cursor = sql_connection.cursor()
     print("Baza danych podłączona")
-    cursor.execute(insert)
-    sql_connection.commit()
+    # cursor.execute(insert)
+    # sql_connection.commit()
+    for row in cursor.execute(select):
+        print(row)  # (1, 'Python', 200.0)
 except sqlite3.Error as e:
     print("Bład podczas otwierania bazy danych", e)
 finally:
